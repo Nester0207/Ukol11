aade1d51d754228193c39934c34d074d810177a9
Serhii Nesterchuk <s.nesterchuk27@gmail.com>
2024-11-25 19:41:52 +0100

    finish

--- js/ukol11-main.js
+++ js/ukol11-main.js
@@ -1,1 +1,18 @@
-let 
+let tlacitkoSubmit = document.querySelectorAll("[name=barva-sabmit]")[0];
+console.log(tlacitkoSubmit);
+
+tlacitkoSubmit.addEventListener("click", (e) => {
+   e.preventDefault();
+
+    let inputBarvaKolecka = document.querySelectorAll("#kocka")[0];
+    console.log(inputBarvaKolecka.value);
+
+    let mujDiv = document.createElement("div"); 
+    mujDiv.classList.add("kolecko");
+
+    let elmKun = document.querySelectorAll(".kun")[0];  
+    elmKun.appendChild(mujDiv);   
+
+    mujDiv.style.backgroundColor = inputBarvaKolecka.value;
+ 
+});
